dac9e57bb1a78f53c5053e6ff66a60898d0d583b
Yuriy Basov <basowy@gmail.com>
2017-07-11 14:14:57 -0700

    1.	Add an onPause method that cancels the timer when the app is paused. 2.	Add Start and Stop buttons that allow the user to start and stop the timer. 3.	Modify the timer so it only executes the timer task every 10 seconds 4.	Added code to the timer task that downloads the RSS feed. 5.	Modify the code that updates the user interface so it displays a message that indicates the number of times that the app has downloaded the RSS feed.

--- app/src/main/java/com/murach/ch10_ex5/MainActivity.java
+++ app/src/main/java/com/murach/ch10_ex5/MainActivity.java
@@ -3,35 +3,82 @@ package com.murach.ch10_ex5;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private RSSFeed feed;
+    private FileIO io;
+
     private TextView messageTextView; 
-    
+    private Timer timer;
+
+    private Button startStopTimerBtn;
+    private Boolean timerRunning = false;
+
+    private int downloadCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        io = new FileIO(getApplicationContext());
         messageTextView = (TextView) findViewById(R.id.messageTextView);
+
+
+        startStopTimerBtn = (Button) findViewById(R.id.startStopTimerBtn);
+
+        startStopTimerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(timerRunning == true){
+                    stopTimer();
+                }
+                else{
+                    startTimer();
+                }
+            }
+        });
         startTimer();
     }
-    
+    private void stopTimer(){
+        timer.cancel();
+        timerRunning = false;
+        toggleStartStopButton();
+    }
+    private void toggleStartStopButton(){
+        if(timerRunning == true){
+            startStopTimerBtn.setText("Stop");
+        }
+        else{
+            startStopTimerBtn.setText("Start");
+        }
+    }
+
     private void startTimer() {
+
+        timerRunning = true;
+
         final long startMillis = System.currentTimeMillis();
-        Timer timer = new Timer(true);
+        timer = new Timer(true);
         TimerTask task = new TimerTask() {
             
             @Override
             public void run() {
                 long elapsedMillis = System.currentTimeMillis() - startMillis;
+                new MainActivity.DownloadFeed().execute();
                 updateView(elapsedMillis);
             }
         };
-        timer.schedule(task, 0, 1000);
+        timer.schedule(task, 0, 10000);
+        toggleStartStopButton();
     }
 
     private void updateView(final long elapsedMillis) {
@@ -42,8 +89,36 @@ public class MainActivity extends Activity {
 
             @Override
             public void run() {
-                messageTextView.setText("Seconds: " + elapsedSeconds);
+                downloadCount++;
+                messageTextView.setText("File downloaded "+downloadCount+" time(s): ");
+
             }
         });
-    }    
+    }
+
+    @Override
+    protected void onPause() {
+
+            timer.purge();
+            timer.cancel();
+
+
+
+        super.onPause();
+    }
+
+    class DownloadFeed extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            io.downloadFile();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.d("News reader", "Feed downloaded");
+        }
+    }
+
+
 }
